5f8992de045ff710a650e4806240ae235f6fa9f5
Serhii Zhukov <sazhukov@gmail.com>
2025-09-04 22:46:50 +0300

    first commit

---
+++ Lab1/main.py
@@ -0,0 +1,15 @@
+
+# Read data (three examples of conditions)
+n1 = float(input("Введіть суму рахунку №1: "))
+n2 = float(input("Введіть суму рахунку №2: "))
+n3 = float(input("Введіть суму рахунку №3: "))
+
+# Calculation for each bill
+p1 = (n1 * 1.10) / 3
+p2 = (n2 * 1.10) / 3
+p3 = (n3 * 1.10) / 3
+
+# Output results (rounding to 2 decimal places)
+print(f"Кожен друг повинен заплатити за рахунок №1: {p1:.2f} грн")
+print(f"Кожен друг повинен заплатити за рахунок №2: {p2:.2f} грн")
+print(f"Кожен друг повинен заплатити за рахунок №3: {p3:.2f} грн")
